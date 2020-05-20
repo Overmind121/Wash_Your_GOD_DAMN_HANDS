c4769e969879fd7578bbbb8a58a4e9e014d68e3e
Overmind121 <lukekim1029@gmail.com>
2020-05-20 12:13:11 -0700

    updated arduino interface

--- WashYourHands/Real_Time_Detection.py
+++ WashYourHands/Real_Time_Detection.py
@@ -1,11 +1,24 @@
 import tensorflow as tf
 import cv2
 import numpy as np
+import serial
+from nanpy import (ArduinoApi, SerialManager)
+from time import sleep
+
+print("Hello World")
+try:
+    connection = SerialManager()
+    a = ArduinoApi(connection = connection)
+except:
+    print("Failed")
+    
+a.pinMode(13, a.OUTPUT)
 
 class_names = ["Not_Washing", "Washing"]
-model_test = tf.keras.models.load_model('/Users/lukek/Wash_Your_GOD_DAMN_HANDS/WashYourHands/Wash_Your_GOD_DAMN_HANDS.h5')
 
-cap = cv2.VideoCapture(r"C:\Users\lukek\Desktop\WashYourGODAMNHANDS\RawFootage\MVI_4059.MP4")
+model_test = tf.keras.models.load_model('/home/pi/Wash_Your_GOD_DAMN_HANDS/WashYourHands/Wash_Your_GOD_DAMN_HANDS.h5')
+
+cap = cv2.VideoCapture(0)
 
 while(True):
     ret, frame = cap.read()
@@ -17,11 +30,16 @@ while(True):
     pred = model_test.predict(numpy_resized)
     print(pred)
     pred_string = class_names[np.argmax(pred)]
-
+    
+    if(pred_string == "Washing"):
+        a.digitalWrite(13, a.HIGH)
+    elif(pred_string == "Not_Washing"):
+        a.digitalWrite(13, a.LOW)
+    
     cv2.putText(frame, pred_string, (100, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,0,0), 2)
 
     cv2.imshow("frame", frame)
-
+    
     if(cv2.waitKey(1) & 0xFF == ord('q')):
         break
 cv2.destroyAllWindows()
